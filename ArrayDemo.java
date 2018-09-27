249f7e551b5c922a3190ad22e2c763d7e6fc6c2f
Mohammad Jawwad <mjawwad@stuy.edu>
2018-09-26 21:48:17 -0400

    Added 0b, half fxnal, but compilable, using nested for loops for rows and columns. However the brackets for each column all bunch up at the beginning

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,4 +1,5 @@
 public class ArrayDemo {
+//0a. Make a function to print a 1D array of ints.
   public static void printArray(int[]ary){
     String arry = "";
     for (int i = 0; i < ary.length; i++){
@@ -9,8 +10,33 @@ public class ArrayDemo {
     }
     System.out.print("["+arry + "]");
   }
+// 0b. Make a function to print a 2d array of ints.
+  public static void printArray(int[][]ary){
+    String arry = "";
+    int row;
+    int column;
+    for (row = 0; row < ary.length; row++){
+      for (column = 0; column < ary[row].length; column ++) {
+        if (column == 0) {
+          System.out.print("[");
+        }
+        if (column != ary[row].length - 1) {
+          arry += ary[row][column] + ", ";
+        }
+        else {
+          arry += ary[row][column] + "]";
+        }}
+        arry += ", "; }
+    System.out.print(arry);
+  }
+
   public static void main(String[] args) {
     int [] test1 = {100,3952,34,1,6};
+    int [][] test2 = {{100,3952},
+                     {34,1},
+                     {6}};
     printArray(test1);
+    System.out.print("\n");
+    printArray(test2);
   }
 }
